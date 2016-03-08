e11e3e65c176f83dafb861f65b24996e78c50a19
Orlando Valdez <orlandox77@gmail.com>
2016-03-07 23:47:09 -0500

    Initial local development

---
+++ fineuploader/fineuploader.go
@@ -0,0 +1,71 @@
+// An upload server for fineuploader.com javascript upload library
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const (
+	// default port number
+	port = 8080
+	// file upload directory
+	uploadDir = "uploads"
+	// fineuploader uuid param name
+	paramUuid = "qquuid"
+	// fineuploader file param name
+	paramFile = "qqfile"
+)
+
+type UploadResponse struct {
+	Success      bool   `json:"success"`
+	Error        string `json:"error,omitempty"`
+	PreventRetry bool   `json:"preventRetry"`
+}
+
+func main() {
+	log.Printf("Initiating server on port [%d]\n", port)
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	http.HandleFunc("/upload", Upload)
+	http.Handle("/delete/", http.StripPrefix("/delete/", http.HandlerFunc(Delete)))
+	log.Fatal(http.ListenAndServe("localhost:"+strconv.Itoa(port), nil))
+}
+
+func Upload(res http.ResponseWriter, req *http.Request) {
+	uplRes := new(UploadResponse)
+
+	uuid := req.FormValue(paramUuid)
+	log.Printf("Trying to save file with uuid of [%s]\n", uuid)
+	file, headers, err := req.FormFile(paramFile)
+	if err != nil {
+		uplRes.Error = err.Error()
+		json.NewEncoder(res).Encode(uplRes)
+		return
+	}
+
+	fileDir := fmt.Sprintf("%s/%s", uploadDir, uuid)
+	os.MkdirAll(fileDir, 0777)
+	filename := fmt.Sprintf("%s/%s", fileDir, headers.Filename)
+
+	outfile, err := os.Create(filename)
+	defer outfile.Close()
+
+	_, err = io.Copy(outfile, file)
+	if err != nil {
+		uplRes.Error = err.Error()
+		json.NewEncoder(res).Encode(uplRes)
+		return
+	}
+
+	uplRes.Success = true
+	json.NewEncoder(res).Encode(uplRes)
+}
+
+func Delete(res http.ResponseWriter, req *http.Request) {
+	log.Printf("Request received [%s]", req.URL.Path)
+}
